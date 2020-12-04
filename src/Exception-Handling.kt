ed70dd1d5ac787d918c807f8929c360225e9ca9e
jannesar.computer@yahoo.com <jannesar.computer@yahoo.com>
2020-12-04 14:50:10 +0330

    Seconde Commit

--- src/Exception-Handling.kt
+++ src/Exception-Handling.kt
@@ -4,7 +4,7 @@ fun main(args:Array<String>){
 
     try {
         val myVar: Int = 12
-        val v:String = "Kotlin Course!"
+        val v:String = " Kotlin Course!"
 //        v.toInt()
 
     }catch (e:Exception){
